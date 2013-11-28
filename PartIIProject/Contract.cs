09338d2dcfc2daf4ef3f6f4f183f1ad576079b7c
aneeshshukla <as2208@cam.ac.uk>
2013-11-28 15:14:48 +0000

    Initialise

---
+++ PartIIProject/Contract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartIIProject
+{
+    class Contract
+    {
+        int contractStartDateIndex; //day number
+        int contractEndDateIndex; //day number
+        int ExerciseFrequency; // days
+        int DecisionFrequency; // days
+        int numberOfComponents; // for the PCA multi-factor model
+        int maxInjectionRate; //barrels
+        int maxWithdrawalRate; //barrels
+        int injectionCost; // $/barrel
+        int withdrawalCost; // $/barrel
+        int volumeUnit; //"we descretize the volume units"
+    }
+}
